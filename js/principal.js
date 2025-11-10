490305e0e1fd53d14abc0edaa94833f8716a78aa
cauasakaue <caua.sakaue@escola.pr.gov.br>
2025-11-10 10:39:06 -0300

    arrumando

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,42 @@
-	var title = document.querySelector("h1");
 
-		//console.log(title);
-		//console.log(title.textContent);
-		title.textContent = "aparecida nutricionista";
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida nutricionista"
+
+//console.log("titulo");
+//console.log("titulo.textContent");
+
+
+/* IMC = massa/altura^2  */
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = document.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhvalido = true;
+var alturaEhvalida = true;
+// validar o peso
+if (peso <=0 || peso >= 1000) {
+    console.log ("Peso inválido")
+    pesoEhvalido = false;
+    tdImc.textContent = "Peso inválido";
+}
+//validar altura
+if (altura <=0 || altura >3) {
+    console.log ("Altura invalida")
+    alturaEhvalida = false;
+    tdImc.textContent = "Altura invalida";
+}
+
+//validar peso e altura
+if (alturaEhvalida && pesoEhvalido) {
+    var imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+}
+
+console.log(paciente); //tr
+console.log(tdPeso); //td que tem peso
+console.log(tdAltura); //td que tem altura
+console.log(peso); // valor do peso
+console.log(altura); // valor da altura
