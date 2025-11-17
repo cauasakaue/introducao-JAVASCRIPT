61cab44a99e521c3645044b2c9b9a02d3a92ad91
cauasakaue <caua.sakaue@escola.pr.gov.br>
2025-11-17 10:53:51 -0300

    colocando função

--- js/form.js
+++ js/form.js
@@ -3,10 +3,8 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(){
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+//Extrai informações do Paciente do form
+    obtemPacienteDoFormulario(form);
 
     var pacienteTr = document.createElement("tr");
     var nomeTd = document.createElement("td");
@@ -19,14 +17,29 @@ botaoAdicionar.addEventListener("click", function(){
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
+    imcTd.testcontent = calculaIMC(peso,altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(IMCTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
 });
+
+// dividir em três funções para trabalhar com tarefas mwnores
+function obtemPacienteDoFormulario(form){
+    var paciente = {
+        nome:
+        peso:
+        altura:
+        gordura:
+    }
+
+    return paciente;
+   
+}
